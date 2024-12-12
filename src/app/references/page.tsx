b46e19da8441db86834fd56600c3e5a47d981784
muhammadkhalid-03 <muhammadkhalid200314@gmail.com>
2024-12-12 10:54:28 -0600

    references

--- src/app/references/page.tsx
+++ src/app/references/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
                 References
             </h1>
             <ul className="text-lg list-[circle] pl-12">
-              <li>https://www.britannica.com/technology/iPhone</li>
-              <li>https://www.forbes.com/sites/traversmark/2023/12/30/the-iphone-effect-are-iphone-users-more-attractive-than-androiders/</li>
-              <li>https://grinnell.primo.exlibrisgroup.com/permalink/01GCL_INST/17ql8au/cdi_informaworld_taylorfrancis_310_1080_17441056_2023_2262870</li>
-              <li>https://www.nytimes.com/2024/10/02/technology/personaltech/apple-intelligence-iphone.html</li>
-              <li>https://www.communicationtheory.org/technological-determinism/</li>
-              <li>https://www.jennyldavis.com/how-artifacts-afford-the-power-and-politics-of-everyday-things</li>
-              <li>http://claireshannon317.blogspot.com/2014/06/articulation-assemblage.html</li>
-              <li>https://www.alice.id.tue.nl/references/pinch-bijker-1984.pdf</li>
-              <li>https://www.digit.in/features/mobile-phones/a-timeline-of-ai-features-on-apple-iphone-the-past-and-the-present.html#:~:text=Apple%20had%20the%20vision%20of,was%20called%20“Knowledge%20Navigator”.</li>
+              <li>Encyclopedia Britannica. “IPhone | Electronic Device.” Encyclopædia Britannica, 31 May 2018, www.britannica.com/technology/iPhone.</li>
+              <li>Travers, Mark. “The “IPhone Effect”: Are IPhone Users More Attractive than Androiders?” Forbes, 30 Dec. 2023, www.forbes.com/sites/traversmark/2023/12/30/the-iphone-effect-are-iphone-users-more-attractive-than-androiders/.</li>
+              <li>Wörsdörfer, Manuel. “Apple's Antitrust Paradox.” European Competition Journal, vol. 20, no. 1, 28 Sept. 2023, pp. 1-34, https://doi.org/10.1080/17441056.2023.2262870.</li>
+              <li>Chen, Brian X. “Apple's A.I. Is Landing Soon on IPhones. Here's What It's Like.” Nytimes.com, The New York Times, 2 Oct. 2024, www.nytimes.com/2024/10/02/technology/personaltech/apple-intelligence-iphone.html.</li>
+              <li>“Technological Determinism.” Communication Theory, 23 Apr. 2018, www.communicationtheory.org/technological-determinism/.</li>
+              <li>Davis, Jenny L. “Jenny L Davis.” Jenny L Davis, 2014, www.jennyldavis.com/how-artifacts-afford-the-power-and-politics-of-everyday-things. Accessed 12 Dec. 2024.</li>
+              <li>Jennifer Daryl Slack, and J Macgregor Wise. Culture and Technology : A Primer. New York, Peter Lang, 2015.</li>
+              <li>Bijker, Wiebe E, et al. The Social Construction of Technological Systems : New Directions in the Sociology and History of Technology. Cambridge, Massachusetts, The Mit Press, 2012.</li>
+              <li>https://www.facebook.com/satvik.pandey19. “A Timeline of AI Features on Apple IPhone: The Past and the Present.” Digit , 9 Sept. 2024, www.digit.in/features/mobile-phones/a-timeline-of-ai-features-on-apple-iphone-the-past-and-the-present.html.</li>
             </ul>
         </main>
     )
